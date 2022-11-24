77ee865e5b26290699ea71f80131fd37bdbbbce0
cyrus <mdoaie@yahoo.co.uk>
2022-11-24 13:24:47 +0000

    xx

--- text.sh
+++ text.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
 
-read -r -p "Are you sure? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-    echo "y"
-else
-   echo "n"
-fi
+read -p " Ici mon texte " continue
