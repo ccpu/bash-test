cdea34ceafdd8f21547f2602567e7d6b5a18f7db
cyrus <mdoaie@yahoo.co.uk>
2022-11-24 13:10:29 +0000

    s

--- text.sh
+++ text.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
-while true; do
-    read -p "Do you wish to install this program?" yn
-    case $yn in
-        [Yy]* ) make install; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+read -p "Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
